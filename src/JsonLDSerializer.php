1006dcf819f571d89b5702c80a2f9bf7f54f8d34
thomas-topway-it <topway.it@mail.com>
2026-09-02 17:39:10 +0400

    add cache

--- src/JsonLDSerializer.php
+++ src/JsonLDSerializer.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @see  https://www.mediawiki.org/wiki/Extension:PageProperties
+ * @see https://www.mediawiki.org/wiki/Extension:PageProperties
  * @author thomas-topway-it for KM-A
  */
 
@@ -10,23 +10,28 @@ namespace SMT;
 use Exception;
 use MediaWiki\Html\Html;
 use MediaWiki\Logger\LoggerFactory;
+use MediaWiki\MediaWikiServices;
 use MediaWiki\Output\OutputPage;
+use MediaWiki\SpecialPage\SpecialPage;
 use MediaWiki\Title\Title;
 use SMW\Exporter\ExporterFactory;
 
 class JsonLDSerializer {
+	 /* @var OutputPage */
+	 private $outputPage;
+
 	/**
 	 * @param Title $title
 	 * @param OutputPage $outputPage
 	 */
 	public function __construct( $title, $outputPage ) {
 		if ( $this->isKnownArticle( $title ) ) {
-			$this->setJsonLD( $title, $outputPage );
+			$this->outputPage = $outputPage;
+			$this->setJsonLD( $title );
 		}
 	}
 
 	/**
-	 * @see https://gerrit.wikimedia.org/r/plugins/gitiles/mediawiki/extensions/PageProperties/+/548d30609c512a79e202dfa7c02a298c66ca34fa/includes/PageProperties.php
 	 * @param Title $title
 	 * @return bool
 	 */
@@ -36,30 +41,46 @@ class JsonLDSerializer {
 	}
 
 	/**
-	 * @see https://gerrit.wikimedia.org/r/plugins/gitiles/mediawiki/extensions/PageProperties/+/548d30609c512a79e202dfa7c02a298c66ca34fa/includes/PageProperties.php
-	 * @param Title $title
-	 * @param OutputPage $outputPage
-	 * @return void
+	 * @return mixed|false
 	 */
-	public static function setJsonLD( $title, $outputPage ) {
-		if ( !class_exists( '\EasyRdf\Graph' ) || !class_exists( '\ML\JsonLD\JsonLD' ) ) {
-			return;
+	private function getCache() {
+		switch ( $GLOBALS['smtgJsonLDCacheStore'] ) {
+			case 'LocalServerObjectCache':
+				return MediaWikiServices::getInstance()->getLocalServerObjectCache();
+
+			case 'WANObjectCache':
+				return MediaWikiServices::getInstance()->getMainWANObjectCache();
+
+			case 'SessionCache':
+			default:
+				// @see MediaWiki\Session\SessionManager
+				$config = MediaWikiServices::getInstance()->getMainConfig();
+				$store = \ObjectCache::getInstance( $config->get(
+					class_exists( 'MainConfigNames' ) ? MainConfigNames::SessionCacheType : 'SessionCacheType' ) );
+				return new \CachedBagOStuff( $store );
 		}
+	}
+
+	/**
+	 * @param Title $title
+	 * @return string|null
+	 */
+	private function getJsonLDOutput( $title ) {
 		// @see SMW\Query\ResultPrinters\RdfResultPrinter
 		$exporterFactory = new ExporterFactory();
 		$serializer = $exporterFactory->newRDFXMLSerializer();
 		$export_controller = $exporterFactory->newExportController( $serializer );
 
-		$outputPage->disable();
-		ob_start();
+		$this->outputPage->disable();
+    	ob_start();
 
 		try {
-			$recursive = true;
-			$export_controller->enableBacklinks( false );
-			$revisionDate = false;
-			$pages = [ $title->getFullText() ];
-			$export_controller->printPages( $pages, $recursive, $revisionDate );
-
+		    $recursive = true;
+		    $export_controller->enableBacklinks( false );
+		    $revisionDate = false;
+		    $pages = [ $title->getFullText() ];
+		    $export_controller->printPages( $pages, $recursive, $revisionDate );
+    
 		} catch ( Exception $e ) {
 			ob_end_clean();
 			LoggerFactory::getInstance( 'smt' )->error( 'SMW ExporterFactory error: ' . $e->getMessage() );
@@ -71,18 +92,57 @@ class JsonLDSerializer {
 		try {
 			$foaf = new \EasyRdf\Graph( $title->getFullUrl(), $data );
 			$format = \EasyRdf\Format::getFormat( 'jsonld' );
-			$output = $foaf->serialise( $format, [
+			return $foaf->serialise( $format, [
 				'compact' => true,
 			] );
 
 		} catch ( Exception $e ) {
 			LoggerFactory::getInstance( 'smt' )->error( 'EasyRdf error: ' . $e->getMessage() );
+		}
+	}
+
+	/**
+	 * @param Title $title
+	 * @return void
+	 */
+	public function setJsonLD( $title ) {
+		if ( !class_exists( '\EasyRdf\Graph' ) || !class_exists( '\ML\JsonLD\JsonLD' ) ) {
 			return;
 		}
 
+		$cache = null;
+		if ( empty( $GLOBALS['smtgJsonLDDisableCache'] ) ) {
+			$revisionId = (int)$title->getLatestRevID();
+			$cache = $this->getCache();
+
+			if ( $cache ) {
+				$cacheKey = $cache->makeKey(
+					'SemanticMetaTags',
+					'jsonld',
+					$title->getNamespace(),
+					$title->getDBkey(),
+					$revisionId,
+				);
+
+				$thisClass = $this;
+				$output = $cache->getWithSetCallback(
+					$cacheKey,
+					$cache::TTL_INDEFINITE,
+					static function () use ( &$thisClass, $title ) {
+						return $thisClass->getJsonLDOutput( $title );
+					}
+				);
+			}
+
+		}
+
+		if ( !$cache ) {		
+			$output = $this->getJsonLDOutput( $title );
+		}
+
 		// https://hotexamples.com/examples/-/EasyRdf_Graph/serialise/php-easyrdf_graph-serialise-method-examples.html
-		if ( is_scalar( $output ) ) {
-			$outputPage->addHeadItem( 'json-ld', Html::Element(
+		if ( is_string( $output ) && !empty( $output ) ) {
+			$this->outputPage->addHeadItem( 'json-ld', Html::Element(
 					'script', [ 'type' => 'application/ld+json' ], $output
 				)
 			);
